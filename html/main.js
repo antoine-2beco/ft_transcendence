4b88419beead32b18e766a650ef5d082c935ee39
antoine-2beco <55329559+antoine-2beco@users.noreply.github.com>
2026-02-04 13:26:01 +0100

    remove old test html

--- html/main.js
+++
@@ -1,6 +0,0 @@
-const backendcheck = document.getElementById('backendcheck');
-
-backendcheck.addEventListener('click', async () => {
-    const res = await fetch('/api/test123');
-    console.log(await res.json());
-});
