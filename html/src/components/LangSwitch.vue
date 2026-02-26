d04d61039e750fc7206a2085db04276a5ab4a72e
antoine-2beco <55329559+antoine-2beco@users.noreply.github.com>
2026-02-26 15:57:36 +0100

    Add Dutch and make a 3 lang switch

--- html/src/components/LangSwitch.vue
+++ html/src/components/LangSwitch.vue
@@ -1,29 +1,39 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import { useI18n } from 'vue-i18n'
 
-const i18n = useI18n();
-const isFr = ref(false);
+const { locale } = useI18n();
+const lang = ref('en');
 
-const toggleLang= () => {
-  isFr.value = !isFr.value;
-  const lang = isFr.value ? 'fr' : 'en';
-  i18n.locale.value = lang;
-  localStorage.setItem('lang', lang);
+const emoji_lang = {
+  'fr' : '🇫🇷',
+  'en' : '🇬🇧',
+  'nl' : '🇳🇱'
+}
+
+const displayLang = computed(() => emoji_lang[lang.value]);
+
+const toggleLang = () => {
+  if (lang.value === 'fr') lang.value = 'en';
+  else if (lang.value === 'en') lang.value = 'nl';
+  else if (lang.value === 'nl') lang.value = 'fr';
+  locale.value = lang.value;
+  localStorage.setItem('lang', lang.value);
 };
 
 onMounted(() => {
   const saved = localStorage.getItem('lang');
-
-  if (saved === 'fr' || !saved) {
-    isFr.value = true;
-    i18n.locale.value = 'fr';
+  if (saved && ['fr', 'en', 'nl'].includes(saved)) {
+    lang.value = saved;
+  } else {
+    lang.value = 'en';
   }
+  locale.value = lang.value;
 });
 </script>
 
 <template>
   <button @click="toggleLang" class="outline contrast">
-    {{ isFr ? '🇫🇷' : '🇬🇧' }}
+    {{ displayLang }}
   </button>
 </template>
--- html/src/locales/index.js
+++ html/src/locales/index.js
@@ -1,6 +1,7 @@
 import { createI18n } from "vue-i18n";
 import en from "./en.json"
 import fr from "./fr.json"
+import nl from "./nl.json"
 
 export const i18n = createI18n({
   locale: 'en',
@@ -8,6 +9,7 @@ export const i18n = createI18n({
   messages: {
     fr,
     en,
+    nl,
   },
 });
 
