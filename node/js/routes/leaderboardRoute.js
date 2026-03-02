d9e7a63f8dcd11424db8cac1cc2164ef61278fb6
Esteban Kina <ekina.pro@gmail.com>
2026-03-02 23:31:57 +0100

    added rank computation to leaderboard

--- node/js/routes/leaderboardRoute.js
+++ node/js/routes/leaderboardRoute.js
@@ -5,6 +5,7 @@ export async function leaderboardRoute(fastify)
     fastify.get("/leaderboard", async (req, reply) => {
         const users = await User.query()
             .select("id", "username", "profile_picture_url", "elo", "wins", "losses", "ties")
+            .select(User.raw("ROW_NUMBER() OVER (ORDER BY elo DESC) as rank"))
             .orderBy("elo", "desc")
             // .limit(50);
     
